6f472f0c303e298af781de34d55ac2462546ba2a
Jeremy Ash <jeremyd.ash@gmail.com>
2020-02-20 12:10:02 -0600

    updated numbers and comments for each step

--- code/create_smoke_outlook.R
+++ code/create_smoke_outlook.R
@@ -30,7 +30,7 @@ burn_lon <- -79.6520
 ## 3. CREATE DIRECTORY TO STORE FILES
 ##-------------
 
-# create directory for burn
+# create directory for burn located in smokeR9/outlooks/burn_dir
 create_md_dir(burn_dir)
 
 
@@ -48,7 +48,7 @@ fp_url(LAT = burn_lat,
 ## 5. DOWNLOAD SCREEN CAPTURE OF FIREPOKER
 ##-------------
 
-# Use Full Page Screen Capture in Chrome to capture met data and download to PNG file. Should download directly to Downloads folder. Move directly to step 6, as it will look for most recently downloaded file. 
+# Use Full Page Screen Capture in Chrome to capture met data and download to PNG file. Should download directly to Downloads folder. Make sure Chrome is open on your laptop (not an external display) and the zoom level is set to 100% (use Ctrl +/- to zoom in and out). Move directly to step 6, as it will look for most recently downloaded file. 
 
 
 
@@ -62,7 +62,7 @@ fp_png_to_burn_dir_fun(burn_dir, model_run)
 
 
 ##-------------
-## 5. CREATE SMOKE OUTLOOK
+## 7. CREATE SMOKE OUTLOOK
 ##-------------
 
 # render smoke template for given burn
@@ -81,7 +81,7 @@ suppressWarnings(
                        ))
 
 ##-------------
-## 6. CLEAR ENVIRONMENT
+## 8. CLEAR ENVIRONMENT
 ##-------------
 
 # make sure to clear environment before running for another burn, as shared variables will need to be re-written
@@ -92,98 +92,6 @@ rm(list = ls())
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 
-
-#############################################################################
-## TESTING WITH MONONGAHELA
-#############################################################################
-
-
-##-------------
-## 1. SOURCE R CODE WITH FUNCTIONS TO GENERATE DIRECTORY, FIREPOKER LINK AND SMOKE REPORT
-##-------------
-
-source("code/render_smoke_report_functions.R")
-
-
-
-##-------------
-## 2. LIST NAME OF FOLDER WHERE BURN INFO WILL BE STORED, WHICH MODEL RUN THIS IS AND BURN COORDINATES
-##-------------
-
-# create directory of burn files in format "YYYY_forest_burnname"
-burn_dir <- "2020_monongahela_testing"
-
-# model run: # needs to be "day_before" or "day_of" 
-model_run <- "day_before"
-
-# burn lat/long
-burn_lat <- 38.7860
-burn_lon <- -79.6520
-
-
-
-##-------------
-## 3. CREATE DIRECTORY TO STORE FILES
-##-------------
-
-# create directory for burn
-create_md_dir(burn_dir)
-
-
-
-##-------------
-## 4. CREATE FIREPOKER LINK TO DOWNLOAD PNG OF WEBPAGE
-##-------------
-
-fp_url(LAT = burn_lat,
-       LON = burn_lon) # coordinates of planned burn
-
-
-
-##-------------
-## 5. DOWNLOAD SCREEN CAPTURE OF FIREPOKER
-##-------------
-
-# Use Full Page Screen Capture in Chrome to capture met data and download to PNG file. Should download directly to Downloads folder. Move directly to step 6, as it will look for most recently downloaded file. 
-
-
-
-##-------------
-## 6. MOVE FIREPOKER IMAGE TO CORRECT FOLDER
-##-------------
-
-# function will move most recently downloaded file (assuming this is the firepoker img) to burn_dir/model_run and rename it to firepoker.PNG
-fp_png_to_burn_dir_fun(burn_dir, model_run)
-
-
-
-##-------------
-## 5. CREATE SMOKE OUTLOOK
-##-------------
-
-# render smoke template for given burn
-suppressWarnings( 
-  render_smoke_report(outlook_path = burn_dir, # update with directory created above
-                      burn_name = "Test", # name of fire
-                      contact_info = "Jeremy Ash, jeremy.ash@usda.gov, 608-234-3300", # full contact info or website
-                      burn_date = "January 31, 2020", # planned date
-                      lat = burn_lat, # latitude of planned ignition
-                      lon = burn_lon, # longitude of planned ignition
-                      model_run = model_run, # created above 
-                      run_id_url = "https://playground-2.airfire.org/bluesky-web-output/15e2aec893afb9-dispersion/", # click on 'Results Output' the Run ID on top left of BSky Dispersion Results and copy full URL
-                      mon_radius = 150, # radius in km around the burn to pull AQI monitors. start with 50 and increase if needed 
-                      drop_low_avg = FALSE,
-                      drop_low_max = TRUE
-  ))
-
-
-
-##-------------
-## 6. CLEAR WORKSPACE
-##-------------
-
-# make sure to clear workspace before running for another burn, as shared variables will need to be re-written
-rm(list = ls())
 
 
 
